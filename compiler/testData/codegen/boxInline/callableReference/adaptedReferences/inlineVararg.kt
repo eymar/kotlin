21732eca4a4af32fb90f0bb25eaad518683deead
Igor Chevdar <igor.chevdar@jetbrains.com>
2021-02-08 20:05:09 +0500

    Fixed test

--- compiler/testData/codegen/boxInline/callableReference/adaptedReferences/inlineVararg.kt
+++ compiler/testData/codegen/boxInline/callableReference/adaptedReferences/inlineVararg.kt
@@ -1,20 +1,20 @@
 // DONT_TARGET_EXACT_BACKEND: WASM
 // WASM_MUTE_REASON: IGNORED_IN_JS
 // !LANGUAGE: +NewInference
-// WITH_RUNTIME
-// KJS_WITH_FULL_RUNTIME
 
 // FILE: 1.kt
 package test
 
-inline fun foo(mkString: (Char, Char) -> String): String =
-        mkString('O','K')
+inline fun foo(f: (Int, Int) -> Int): Int =
+        f(42, 117)
 
-inline fun bar (vararg xs: Char) =
-        String(xs)
+inline fun bar (vararg xs: Int): Int {
+        var sum = 0
+        for (x in xs) sum += x
+        return sum
+}
 
 // FILE: 2.kt
 import test.*
 
-fun box(): String = foo(::bar)
-// -> { a, b -> bar(a, b) }
+fun box(): String = if (foo(::bar) == 159) "OK" else "fail"
